3430544fcfc13b48a1a9fd9cd790a90ace20b17e
Stefano <stefano.salsano@uniroma2.it>
2020-05-06 12:45:08 +0200

    wip

--- packer/start-xubuntu-vagrant-vbox-from-box.sh
+++ packer/start-xubuntu-vagrant-vbox-from-box.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+sudo bash << EOF
 # User rose needs to sudo without password
-sudo echo "rose        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/rose
-sudo chmod 0440 /etc/sudoers.d/rose
+echo "rose        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/rose
+chmod 0440 /etc/sudoers.d/rose
 
-sudo chmod +x /tmp/execute-xubuntu-vagrant-vbox-from-box.sh
+
+chmod +x /tmp/execute-xubuntu-vagrant-vbox-from-box.sh
+EOF
+
 sudo -i -u rose bash /tmp/execute-xubuntu-vagrant-vbox-from-box.sh
 
