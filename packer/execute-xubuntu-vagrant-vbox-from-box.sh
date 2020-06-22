e61d12e063897423573afda1e5d3551c31ec8b4d
Francesco Lombardo <franclombardo@gmail.com>
2020-06-22 12:02:56 +0200

    force update

--- packer/execute-xubuntu-vagrant-vbox-from-box.sh
+++ packer/execute-xubuntu-vagrant-vbox-from-box.sh
@@ -23,9 +23,11 @@ mkdir -p "$WORKSPACE_DIR"
 mkdir -p "$ROSE_SYS_DIR"
 
 # Update apt
-sudo apt update
+sudo apt-get update
 
-sudo apt autoremove -y
+sudo apt-get upgrade -y
+
+sudo apt-get autoremove -y
 
 sudo apt-get install -y git-core
 
@@ -71,7 +73,7 @@ echo -e "\n\n#####################################"
 echo -e "\n-Installing sublime evaluation version"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
+sudo apt-get update
 sudo apt-get install -y sublime-text
 
 # Install tcpdump
