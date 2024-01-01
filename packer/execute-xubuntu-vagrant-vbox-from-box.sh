6bfd8c1e7aac6b987806be53f9c2d0cdc20c4dd4
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-01-01 18:14:49 +0100

    Update execute-xubuntu-vagrant-vbox-from-box.sh

--- packer/execute-xubuntu-vagrant-vbox-from-box.sh
+++ packer/execute-xubuntu-vagrant-vbox-from-box.sh
@@ -25,7 +25,7 @@ mkdir -p "$ROSE_SYS_DIR"
 # Update apt
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y
 
 sudo apt-get autoremove -y
 
@@ -58,17 +58,18 @@ rm google-chrome-stable_current_amd64.deb
 # Install sublime evaluation version
 echo -e "\n\n#####################################"
 echo -e "\n-Installing sublime evaluation version"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install -y sublime-text
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sublime-text
 
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
 
 # Install wireshark
 echo -e "\n\n#####################################"
 echo -e "\n-Installing wireshark"
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y wireshark
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y wireshark
 
 echo -e "\n\n#####################################"
 echo -e "\n- Terminal config"
